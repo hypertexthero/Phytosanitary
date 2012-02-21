9a1ed425e62510cefb24c31acfafa87cd2b101ef
Simon Griffee <simon@hypertexthero.com>
2012-02-21 10:23:35 +0100

    removed deleted folders

--- coltrane/urls/categories.py
+++
@@ -1,9 +0,0 @@
-# from django.conf.urls.defaults import *
-# 
-# from phytosanitary.models import Category
-# 
-# urlpatterns = patterns('',
-#     # (r'^$', 'django.views.generic.list_detail.object_list', { 'queryset': Category.objects.all() }, 'phytosanitary_category_list'),
-#     # moving this to phytosanitary/urls.py
-#     # (r'^(?P<slug>[-\w]+)/$', 'phytosanitary.views.category_detail', {}, 'phytosanitary_category_detail'),
-# )
