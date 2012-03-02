56c162e453e8e11c8781d5af606f5e048c4330d4
Simon Griffee <simon@hypertexthero.com>
2012-03-02 11:39:52 +0100

    removing sites framework for testing

--- settings.py
+++ settings.py
@@ -118,7 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
+    # 'django.contrib.sites', # don't need this as using one site only
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.comments',
