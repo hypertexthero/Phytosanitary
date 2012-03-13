ecb4bb2f30acdddc71d0174cbb5a96d654bad28c
Simon Griffee <simon@hypertexthero.com>
2012-03-13 15:44:01 +0100

    updated form markup and css

--- phytosanitary/models.py
+++ phytosanitary/models.py
@@ -16,6 +16,7 @@ from django.dispatch import receiver # https://docs.djangoproject.com/en/dev/top
 from userena.models import UserenaBaseProfile
 
 class Contributor(UserenaBaseProfile):
+    """ Contributor user profiles """
     user = models.OneToOneField(User,
                                 unique=True,
                                 verbose_name=_('user'),
@@ -68,6 +69,7 @@ class Category(models.Model):
 
 
 class LiveResourceManager(models.Manager):
+    """ Live and For Review resource status """
     def get_query_set(self):
         return super(LiveResourceManager, self).get_query_set().filter(status=self.model.LIVE_STATUS)
 
@@ -77,6 +79,7 @@ class LiveResourceManager(models.Manager):
     # return '/%Y/%m/'.join(['content', instance.user.username, filename])
 
 class Resource(models.Model):
+    """ Phytosanitary Resource """
     LIVE_STATUS = 1
     FOR_REVIEW_STATUS = 2
     STATUS_CHOICES = (
@@ -96,11 +99,7 @@ class Resource(models.Model):
     body = models.TextField(blank=False, help_text='Use <a href="/markdown/" onclick="window.open(this.href, this.target); return false;">Markdown format</a>', verbose_name='Description')
     pub_date = models.DateTimeField(default=datetime.datetime.now, verbose_name='Publication Date', help_text='(will only be published when approved by an administrator)')
     org_title = models.CharField(blank=True, max_length=250, help_text='', verbose_name='Organization')
-    
-    # *************************
-    # items = MultiuploaderImage.objects.all()
-    
-    # OLD - do not usefile = models.FileField('Upload', upload_to='files/%Y/%m%d%H%M%S/')
+
     url = models.URLField(blank=True, help_text="A link to something elsewhere.", verbose_name='URL')
     contact_type = models.CharField(blank=True, max_length=1, choices=CONTACT_TYPE_CHOICES, default=1, verbose_name='Type of Contact')
     contact_email = models.EmailField(blank=True, verbose_name='Email of Contact')
@@ -154,7 +153,6 @@ class Resource(models.Model):
     def filename(self):
         return os.path.basename(self.document.name)
 
-
 # tagging users django-tagging
 # See http://blog.sveri.de/index.php?/archives/139-django-tagging.html
 # With bugfix workaround - http://stackoverflow.com/questions/6295104/django-tagging-already-registered-exception
@@ -163,15 +161,11 @@ try:
 except tagging.AlreadyRegistered:
     pass
 
-
 class Document(models.Model):
+    """ Documents """
     resource = models.ForeignKey(Resource) # , related_name='photos'
-    # keep = models.BooleanField(default=True, editable=False)
-    # remove = models.BooleanField(default=False)
-    # title = models.CharField(blank=True, null=True, max_length=100)
     # http://stackoverflow.com/a/1190866/412329
     document = models.FileField(blank=True, help_text='10 MB maximum file size.', verbose_name='Upload a file', upload_to='files/%Y/%m/%d/')
-    # caption = models.CharField(blank=True, max_length=250)
 
     # Eureka!! http://scottbarnham.com/blog/2008/02/24/imagefield-and-edit_inline-revisited/   
     def save(self):
@@ -197,10 +191,8 @@ class Document(models.Model):
     #     return ('phytosanitary_resource_detail', None, {'object_id': self.id})
 
 class Photo(models.Model):
+    """ Photos """
     resource = models.ForeignKey(Resource) # , related_name='photos'
-    # keep = models.BooleanField(default=True, editable=False)
-    # remove = models.BooleanField(default=False)
-    # title = models.CharField(blank=True, null=True, max_length=100)
     image = models.ImageField(blank=True, upload_to="photos/%Y/%m/%d/") 
     caption = models.CharField(blank=True, max_length=250)
 
@@ -226,14 +218,8 @@ class Photo(models.Model):
 
 from django import forms
 from django.forms import ModelForm, Textarea
-# from django.forms.fields import DateField, ChoiceField, MultipleChoiceField
 class ResourceForm(forms.ModelForm):
     agreement = forms.BooleanField(required=True, label='I agree to have these Phytosanitary Technical Resources published in public')
-    # http://stackoverflow.com/questions/5871730/need-a-minimal-django-file-upload-example
-    # document = forms.FileField(label='Select a file to upload',help_text='max. 10 megabytes', required=False)
-    # description = forms.Textarea(widget=forms.Textarea)
-    # http://stackoverflow.com/a/2734790/412329
-    # categories = forms.MultipleChoiceField(queryset=Category.objects.all(), widget=forms.CheckboxSelectMultiple, label="Categories", required=True)
     
     required_css_class = 'required'
     class Meta:
@@ -244,135 +230,13 @@ class ResourceForm(forms.ModelForm):
                 }
 
 class PhotoForm(forms.ModelForm):
-    # resource = forms.ForeignKey(Resource)
-    # title = forms.CharField(max_length=100)
-    # image = forms.ImageField()
-    # caption = forms.CharField(max_length=250)
     class Meta:
         model = Photo
         fields = ('image', 'caption',)
         exclude = ('resource',)
 
 class DocumentForm(forms.ModelForm):
-    # resource = forms.ForeignKey(Resource)
-    # title = forms.CharField(max_length=100)
-    # image = forms.ImageField()
-    # caption = forms.CharField(max_length=250)
     class Meta:
         model = Document
         fields = ('document',)
         exclude = ('resource',)
-
-# FOR FUTURE USE:
-
-# class Link(models.Model):
-#     # Metadata.
-#     enable_comments = models.BooleanField(default=True)
-#     post_elsewhere = models.BooleanField('Post to Delicious', default=True, help_text='If checked, this will be posted both to your weblog and to your delicious.com account. (Currently disabled)')
-#     posted_by = models.ForeignKey(User)
-#     pub_date = models.DateTimeField(default=datetime.datetime.now)
-#     slug = models.SlugField(unique_for_date='pub_date', help_text='Must be unique for the publication date.')
-#     title = models.CharField(max_length=250)
-#     
-#     # The actual link bits.
-#     description = models.TextField(blank=True)
-#     description_html = models.TextField(editable=False, blank=True)
-#     via_name = models.CharField('Via', max_length=250, blank=True, help_text='The name of the person whose site you spotted the link on. Optional.')
-#     via_url = models.URLField('Via URL', blank=True, help_text='The URL of the site where you spotted the link. Optional.')
-#     url = models.URLField('URL', unique=True)
-#     tags = TagField()
-#     
-#     class Meta:
-#         ordering = ['-pub_date']
-#         
-#     def __unicode__(self):
-#         return self.title
-#     
-#     def save(self):
-#         if not self.id and self.post_elsewhere:
-#             import pydelicious
-#             from django.utils.encoding import smart_str
-#             pydelicious.add(settings.DELICIOUS_USER, 
-#                             settings.DELICIOUS_PASSWORD, 
-#                             smart_str(self.url), 
-#                             smart_str(self.title), 
-#                             smart_str(self.tags))
-#         if self.description:
-#             self.description_html = markdown(self.description)
-#         super(Link, self).save()
-#     
-#     @models.permalink
-#     def get_absolute_url(self):
-#         return ('phytosanitary_link_detail', (), {   'year': self.pub_date.strftime('%Y'),
-#                                                 'month': self.pub_date.strftime('%b').lower(),
-#                                                 'day': self.pub_date.strftime('%d'),
-#                                                 'slug': self.slug })
-
-# tagging users django-tagging
-# See http://blog.sveri.de/index.php?/categories/2-Django
-# tagging.register(Link, tag_descriptor_attr='etags')
-
-
-
-# The first comment moderation system...
-#
-# from akismet import Akismet
-# from django.conf import settings
-# from django.contrib.comments.models import Comment
-# from django.contrib.comments.signals import comment_will_be_posted
-# from django.contrib.sites.models import Site
-# from django.core.mail import mail_managers
-# from django.utils.encoding import smart_str
-# 
-# def moderate_comment(sender, comment, request, **kwargs):
-#     if not comment.id:
-#         resource = comment.content_object
-#         delta = datetime.datetime.now() - resource.pub_date
-#         if delta.days > 30:
-#             comment.is_public = False
-#         else:
-#             akismet_api = Akismet(key=settings.AKISMET_API_KEY, blog_url="http:/%s/" %Site.objects.get_current().domain)
-#             if akismet_api.verify_key():
-#                 akismet_data = { 'comment_type': 'comment',
-#                                  'referrer': request.META['HTTP_REFERER'],
-#                                  'user_ip': comment.ip_address,
-#                                  'user-agent': request.META['HTTP_USER_AGENT'] }
-#                 if akismet_api.comment_check(smart_str(comment.comment),
-#                                             akismet_data,
-#                                             build_data=True):
-#                     comment.is_public = False
-#         email_body = "%s posted a new comment on the resource '%s'."
-#         mail_managers("New comment posted", email_body % (comment.name, comment.content_object))
-#                     
-# comment_will_be_posted.connect(moderate_comment, sender=Comment)
-
-
-# The second comment moderation system...
-
-# from akismet import Akismet
-# from django.conf import settings
-# from django.contrib.comments.moderation import CommentModerator, moderator
-# from django.contrib.sites.models import Site
-# from django.utils.encoding import smart_str
-# 
-# class ResourceModerator(CommentModerator):
-#     auto_moderate_field = 'pub_date'
-#     moderate_after = 30
-#     email_notification = True
-#     
-#     def moderate (self, comment, content_object, request):
-#         already_moderated = super(ResourceModerator, self).moderate(comment, content_object, request)
-#         if already_moderated:
-#             return True
-#         akismet_api = Akismet(key=settings.AKISMET_API_KEY, blog_url="http:/%s/" %Site.objects.get_current().domain)
-#         if akismet_api.verify_key():
-#             akismet_data = { 'comment_type': 'comment',
-#                              'referrer': request.META['HTTP_REFERER'],
-#                              'user_ip': comment.ip_address,
-#                              'user-agent': request.META['HTTP_USER_AGENT'] }
-#             return akismet_api.comment_check(smart_str(comment.comment),
-#                                 akismet_data,
-#                                 build_data=True)
-#         return False
-#         
-# moderator.register(Resource, ResourceModerator)
--- phytosanitary/views.py
+++ phytosanitary/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from phytosanitary.models import Category, Resource, Contributor
 from django.views.generic.list_detail import object_list
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from models import Resource, ResourceForm, Contributor, Document, DocumentForm, Photo, PhotoForm
+from django import forms
 
 def category_detail(request, slug):
     """ categories are the site sections i.e. the global navigation """
@@ -14,38 +19,14 @@ def category_detail(request, slug):
         }
     )
 
-
 # http://stackoverflow.com/questions/907858/how-to-let-djangos-generic-view-use-a-form-with-initial-values
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.http import HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
-from models import Resource, ResourceForm, Contributor, Document, DocumentForm, Photo, PhotoForm
-from django import forms
-
-# def handle_uploaded_file(f):
-#     destination = open('some/file/name.txt', 'wb+')
-#     for chunk in f.chunks():
-#         destination.write(chunk)
-#     destination.close()
-# 
-# import tempfile
-# import shutil
-
-# FILE_UPLOAD_DIR = '/home/imran/uploads'
-# def handle_uploaded_file(source):
-#     fd, filepath = tempfile.mkstemp(prefix=source.name, dir=MEDIA_ROOT)
-#     with open(filepath, 'wb') as dest:
-#         shutil.copyfileobj(source, dest)
-#     return filepath
 
 # resource_upload.html
 # http://stackoverflow.com/questions/569468/django-multiple-models-in-one-template-using-forms
 # http://collingrady.wordpress.com/2008/02/18/editing-multiple-objects-in-django-with-newforms/
+@login_required
 def resource_upload(request):
     if request.method == "POST":
-        
-        # object_id = Resource.objects.all()
         
         resourceform = ResourceForm(request.POST, request.FILES, instance=Resource())
         photoform = [PhotoForm(request.POST, request.FILES, prefix=str(x), instance=Photo()) for x in range(0,10)]
@@ -68,8 +49,6 @@ def resource_upload(request):
                 df.save()
             return HttpResponseRedirect('/thanks/')
     else:
-        # resourceform = ResourceForm()
-        # photoform = [PhotoForm(prefix=str(x)) for x in range(0,3)]
         resourceform = ResourceForm(instance=Resource())
         photoform = [PhotoForm(prefix=str(x), instance=Photo()) for x in range(0,10)]
         documentform = [DocumentForm(prefix=str(x), instance=Document()) for x in range(0,10)]
@@ -83,12 +62,7 @@ from django.core.urlresolvers import reverse
 import datetime
 import time
 from django.db.models.fields import DateTimeField
-# def get_photos(): 
-#    return Photo.objects.all()
-
 from django.views.generic import date_based 
-# from yourproj.yourapp.models import Thing1 
-# from yourproj.yourotherapp.models import Thing2 
 def phytosanitary_resource_detail(request, year, month, day, slug): 
     q1 = Resource.objects.all() 
     q2 = Photo.objects.all()
@@ -152,19 +126,3 @@ def search(request):
             'results': results,
             'user': user
              })
-
-# =todo: upload resources form for contributors
-# - http://parand.com/say/index.php/2010/02/19/django-using-the-permission-system/:
-def upload():
-    """
-        upload resources form
-    
-        Resource publication [moderation](https://github.com/dominno/django-moderation#readme) - permissions Groups for contributors, moderators, secretariat staff
-            - Non-secretariat users who register can only submit resources for approval by a moderator or chief administrator, not publish directly to the site.
-                - <del>[Users who register through front-end of site need to be automatically assigned to 'contributors' group](http://stackoverflow.com/questions/8949303/how-to-assign-a-user-to-a-group-at-signup-using-django-userena)</del> Done.
-                - *Contributors can add/save a resource as Draft/for review only*
-            - [Email](http://stackoverflow.com/questions/2349483/django-models-signals-and-email-sending-delay) [notification](https://github.com/jtauber/django-notification/) to moderators/staff when new resources are submitted
-    
-    
-    """
-    pass
